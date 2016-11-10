a158d8cb3adf3644fc759b7b2e9c580c6709aa31
Beatriz de Miguel <beatriz.demiguel@bbva.com>
2016-11-10 18:42:03 +0100

    add model to restaurants route

--- foodme/app/routes/restaurants.js
+++ foodme/app/routes/restaurants.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
+const host = 'https://raw.githubusercontent.com/shokmaster/ember-workshops/master/';
+const restaurantsUrl = `${host}resources/restaurants.json`;
+
 export default Ember.Route.extend({
+
+	model: function() {
+		return Ember.$.getJSON(restaurantsUrl);
+	}
 });
