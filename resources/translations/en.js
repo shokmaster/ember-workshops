a50ea9b0693c20c4329bb9891fb50039e5301f7c
Beatriz de Miguel <beatriz.demiguel@bbva.com>
2016-11-10 18:20:05 +0100

    add more resources: translation strings

---
+++ resources/translations/en.js
@@ -0,0 +1,13 @@
+{
+	"about": "About",
+	"language": "Language",
+	"restaurants": "Restaurants",
+	"foundRestaurants": {
+		"zero": "No restaurants found!",
+		"one": "{{count}} restaurant found!",
+   		"other": "{{count}} restaurants found!"
+	},
+	"filterRestaurants": "Filter restaurants",
+	"filter": "Filter",
+	"cuisine": "Cuisine"
+}
---
+++ resources/translations/es.js
@@ -0,0 +1,13 @@
+{
+	"about": "Sobre nosotros",
+	"language": "Idioma",
+	"restaurants": "Restaurantes",
+	"foundRestaurants": {
+		"zero": "¡No se ha encontrado ningún restaurante!",
+		"one": "¡{{count}} restaurante encontrado!",
+   		"other": "¡{{count}} restaurantes encontrados!"
+	},
+	"filterRestaurants": "Filtra restaurantes",
+	"filter": "Filtrar",
+	"cuisine": "Cocina"
+}
