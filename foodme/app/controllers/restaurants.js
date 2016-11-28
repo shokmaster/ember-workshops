de118b3decb3ff7d2b3200abcc5fafbd1470eb3c
beatriz de miguel <beatriz.demiguel@bbva.com>
2016-11-28 21:51:25 +0100

    improvements

--- foodme/app/controllers/restaurants.js
+++ foodme/app/controllers/restaurants.js
@@ -22,30 +22,26 @@ export default Ember.Controller.extend({
 
 	selectedCuisins: [],
 
-	filteredRestaurants: Ember.computed('model', function() {
-		return this.get('model');
+	filterRating: null,
+
+	filteredRestaurants: Ember.computed('cuisinesFilters', 'model', 'filterRating', function() {
+		let filteredRestaurants = this.get('model');
+		const cuisinesFilters =  this.get('selectedCuisins');
+		const filterRating = this.get('filterRating');
+
+		if (!Ember.isEmpty(cuisinesFilters)) {
+			filteredRestaurants = filteredRestaurants.filter((item) => {
+				return cuisinesFilters.mapBy('name').includes(item.cuisine);
+			});
+		}
+
+		if (filterRating){
+			filteredRestaurants = filteredRestaurants.filterBy('rating', filterRating);
+		}
+		return filteredRestaurants;
 	}),
 
-	filterRating: 0,
-
 	actions: {
-		filter(){
-			let filteredRestaurants = this.get('model');
-			const cuisinesFilters =  this.get('selectedCuisins');
-			const filterRating = this.get('filterRating');
-
-			if (!Ember.isEmpty(cuisinesFilters)) {
-				filteredRestaurants = filteredRestaurants.filter((item) => {
-					return cuisinesFilters.mapBy('name').includes(item.cuisine);
-				});
-			}
-
-			if (filterRating){
-				filteredRestaurants = filteredRestaurants.filterBy('rating', filterRating);
-			}
-
-			this.set('filteredRestaurants', filteredRestaurants);
-		},
 
 		updateRating(stars){
 			this.set('filterRating',stars.rating);
